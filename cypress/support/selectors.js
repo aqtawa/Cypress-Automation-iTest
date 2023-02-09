b9130f2432ac25fa60c1b67a7166c13a31251aed
Timur <dangerbit1999@gmail.com>
2023-02-09 18:47:34 +0600

    fix latest pipeline fail

--- cypress/support/selectors.js
+++ cypress/support/selectors.js
@@ -1,5 +1,5 @@
 export default {
-  historyKzBtn: '[href="/ru/ent/istoriya-kazahstana"] > .subject-block__image > .subject-block__icon',
+  historyKzBtn: '[href="/ru/ent/istoriya-kazahstana"] > .subject-block__image',
   firstConspectHistoryKz: '#clone_lectures > .list-of-chapters > :nth-child(1) > .chapter-block__button-group > .button_theme_conspect',
   firstTestHistoryKz: '#clone_lectures > .list-of-chapters > :nth-child(1) > .chapter-block__button-group > .button_theme_test',
 };
