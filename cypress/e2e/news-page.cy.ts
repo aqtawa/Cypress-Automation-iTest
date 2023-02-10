3bcc5cdcfa661845accce7a73ca4f60392ce3e30
Timur <dangerbit1999@gmail.com>
2023-02-10 18:10:40 +0600

    add news page

---
+++ cypress/e2e/news-page.cy.ts
@@ -0,0 +1,37 @@
+describe('Navigation', () => {
+    beforeEach(() => {
+        cy.visit('https://itest.dev-bmg.kz/ru/news')
+    })
+
+    it('Click on news block', () => {
+        cy.get('.article-block__preview > img').eq(0).then($newsBlock => {
+            cy.get('.articles-list > a').eq(0).click()
+            cy.get('.breadcrumbs > :nth-child(3) > span').should('have.text', $newsBlock.prop('alt'))
+            cy.go('back')
+        })
+        cy.get('.article-block__preview > img').eq(1).then($newsBlock => {
+            cy.get('.articles-list > a').eq(1).click()
+            cy.get('.breadcrumbs > :nth-child(3) > span').should('have.text', $newsBlock.prop('alt'))
+        })
+    });
+
+    it('Page selection', () => {
+        cy.get('.page-link').eq(2).click()
+        cy.location('href').should('eq', 'https://itest.dev-bmg.kz/ru/news?page=2')
+        cy.get('.page-link').eq(0).click()
+        cy.location('href').should('eq', 'https://itest.dev-bmg.kz/ru/news?page=1')
+        cy.get('.page-link').eq(12).click()
+        cy.location('href').should('eq', 'https://itest.dev-bmg.kz/ru/news?page=2')
+    });
+})
+
+
+
+
+
+
+
+
+
+
+export{}
